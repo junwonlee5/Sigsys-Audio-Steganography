7f2f4053699f3188f778e6186b55d8050c518aa0
Kristtiya <kristtiyaguerra3.14@gmail.com>
2019-05-02 22:30:43 -0400

    Add working LSB.py code

--- Algorithms/LSB.py
+++ Algorithms/LSB.py
@@ -18,10 +18,8 @@ def convertmessage(message): #Converts message to a binary form.
     for x in message:
         bin_msg.append(bin(ord(x))[2:].zfill(8))
     bin_msg = "".join(bin_msg)
-
     for ch in bin_msg:
         fmsg.append(ch)
-    
     return fmsg
     
 def binary_audio(data):  ## Returns the audio in binary
@@ -29,7 +27,7 @@ def binary_audio(data):  ## Returns the audio in binary
     for i in range(len(data)):
         binary = bin(data[i])
         binarydata.append(str(binary))
-        
+
     return binarydata
 
 def encode_data(binarydata, message):  ##encodes message into file using LSB
@@ -46,10 +44,12 @@ def encode_data(binarydata, message):  ##encodes message into file using LSB
         nob = binarydata[i]
         nob = nob.replace('0b','')
         encoded.append(nob)
+
+    encodedfile = open("encodedLSB.txt","w")
+    encodedfile.write(str(encoded).replace('0b',''))
     return encoded
 
-
-def plot_encoded_file(filename, rate, time, data):  ## Plots and saves encoded audio file
+def plot_encoded_file(filename,time, rate, data):  ## Plots and saves encoded audio file
     newdata = []
     for i in range(len(data)):
         data[i] = data[i].replace('b','')
@@ -57,25 +57,32 @@ def plot_encoded_file(filename, rate, time, data):  ## Plots and saves encoded a
     plt.plot(time, newdata)
     plt.xlabel('Time (s)')
     plt.ylabel('Amplitude')
-    plt.title('Encoded AUdio FIle')
+    plt.title('Encoded Audio File')
     plt.savefig('EncodedAudiofile-LSB.png')
-    wav.write('LSB_modified'+str(filename), rate, data)
+    # wavefile.write('LSB modified'+str(filename) , rate, data)
+    arr = np.int16(newdata)
+    wav.write('LSB_modified'+str(filename), 44100, arr)
 
-
-def decode(encodeddata):
+def decode(filename):
+    binary = []
     message = []
     lsb = []
-    for i in range(40):
-        lsb.append(encodeddata[i][-1])
-    
+    dataq = []
+    rate, data = wav.read(filename)
+    data.tolist()
+    for i in range(len(data)):
+        binary.append(bin(data[i]))
+    for i in range(len(binary)):
+        lsb.append(binary[i][-1])
     lsb = ''.join(lsb)
-
-    n = 8
-    lsb = [lsb[i:i+n] for i in range(0,len(lsb),n)]
-    print(lsb)
+    lsb = [lsb[i:i+8] for i in range(0,len(lsb),8)]
     for i in range(len(lsb)):
         message.append(chr(int(lsb[i], 2)))
-    print(message[0:100])
+    message = list(message)
+    message = ''.join(message)
+    decodedfile = open("decodedfile.txt","w")
+    decodedfile.write(str(message))
+    
 
 
 def encode(filename, message):
@@ -83,8 +90,7 @@ def encode(filename, message):
     bin_msg = convertmessage(message)
     binarydata = binary_audio(data)
     encoded = encode_data(binarydata,bin_msg)
-    plot_encoded_file(filename, rate, time_array,encoded)
-    decode(encoded)
-
-    # decode(encoded)
-encode("bell1.wav", "abcde")
+    plot_encoded_file(filename, time_array,rate,encoded)
+    return encoded
+encode("bell1.wav", "According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyways. Because bees don't care what humans think is impossible.")
+decode('LSB_modifiedbell1.wav')
